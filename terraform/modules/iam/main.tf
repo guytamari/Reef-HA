77b68a1d1de80ba82edd7a6de359348e5bb19210
guytamari <guy8267@gmail.com>
2025-05-08 00:06:41 +0300

    modify iam and backend

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -1,27 +1,4 @@
-# ECS Task Execution Role
-resource "aws_iam_role" "ecs_task_execution" {
-  name = "${var.project_name}-ecs-task-execution-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
-  role       = aws_iam_role.ecs_task_execution.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-# ECS Execution Role
+# ECS Task Execution Role - Used by ECS to pull images and write logs
 resource "aws_iam_role" "ecs_execution" {
   name = "${var.project_name}-ecs-execution-role"
 
@@ -43,12 +20,13 @@ resource "aws_iam_role" "ecs_execution" {
   }
 }
 
+# Attach the AWS managed policy for ECS task execution
 resource "aws_iam_role_policy_attachment" "ecs_execution" {
   role       = aws_iam_role.ecs_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-# ECS Task Role
+# ECS Task Role - Used by your application running in the container
 resource "aws_iam_role" "ecs_task" {
   name = "${var.project_name}-ecs-task-role"
 
@@ -70,29 +48,14 @@ resource "aws_iam_role" "ecs_task" {
   }
 }
 
-resource "aws_iam_role_policy" "ecs_task" {
-  name = "${var.project_name}-ecs-task-policy"
-  role = aws_iam_role.ecs_task.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "ecr:GetAuthorizationToken",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:BatchGetImage"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
+# Attach CloudWatch Logs policy to allow the task to write logs
+resource "aws_iam_role_policy_attachment" "ecs_logs" {
+  role       = aws_iam_role.ecs_task.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
 }
 
-# attachment
-resource "aws_iam_role_policy_attachment" "ecs_logs_attach" {
-  role       = aws_iam_role.ecs_task_execution.name
-  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+# Attach ECR policy to allow the task to pull images
+resource "aws_iam_role_policy_attachment" "ecs_ecr" {
+  role       = aws_iam_role.ecs_task.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonECR-FullAccess"
 }
