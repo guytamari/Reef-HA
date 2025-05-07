74e26dffdc4fa80e33fd62dd0d28d9d2d49b2613
guytamari <guy8267@gmail.com>
2025-05-07 22:44:38 +0300

    added tfvars example and modify the ci cd

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,27 @@
 output "ecr_repository_url" {
   value = module.ecr.repository_url
 }
+output "ecr_repository_name" {
+  value = module.ecr.repository_name
+}
+
+
+output "ecs_cluster_name" {
+  value = module.ecs.cluster_name
+}
+
+output "ecs_service_name" {
+  value = module.ecs.service_name
+}
+
+output "alb_dns_name" {
+  value = module.alb.alb_dns_name
+}
+
+output "alb_zone_id" {
+  value = module.alb.alb_zone_id
+}
+
+output "https_url" {
+  value = "https://${var.domain_name}"
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,54 +1,45 @@
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
 }
 
 
 variable "vpc_cidr" {
   description = "CIDR block for VPC"
   type        = string
-  default     = "10.0.0.0/16"
 }
 
 variable "availability_zones" {
   description = "List of availability zones"
   type        = list(string)
-  default     = ["us-east-1a", "us-east-1b"]
 }
 
 variable "public_subnet_cidrs" {
   description = "CIDR blocks for public subnets"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   description = "CIDR blocks for private subnets"
   type        = list(string)
-  default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "repository_name" {
   description = "Name of the ECR repository"
   type        = string
-  default     = "hello-world-app"
 }
 
 variable "domain_name" {
   description = "Domain name for the application"
   type        = string
-  default     = "hello-world-reef.com"
 }
 
 variable "container_port" {
   description = "Port the container listens on (used in ECS)"
   type        = number
-  default     = 80
 }
 
 variable "project_name" {
   description = "default project name"
   type        = string
-  default     = "reef-ha-hello"
 }
