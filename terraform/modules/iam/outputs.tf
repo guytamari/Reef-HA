bf78f7fa419d3153e27873556ad818a750761945
guytamari <guy8267@gmail.com>
2025-05-08 00:26:00 +0300

    Update outputs.tf

--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -1,6 +1,6 @@
 output "ecs_task_execution_role_arn" {
   description = "ARN of ECS task execution role"
-  value       = aws_iam_role.ecs_task_execution.arn
+  value       = aws_iam_role.ecs_execution.arn
 }
 
 output "ecs_execution_role_arn" {
